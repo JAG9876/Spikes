e539b8f69d75504a5823b8d5c624fe6fba81a525
JAG7070 <pej@crawlsoft.com>
2025-12-13 01:03:43 +0100

    Less code - same result.

--- Google/SignIn/ClientGoogleSignIn/app/src/main/java/com/example/clientgooglesignin/MainActivity.kt
+++ Google/SignIn/ClientGoogleSignIn/app/src/main/java/com/example/clientgooglesignin/MainActivity.kt
@@ -1,27 +1,23 @@
 package com.example.clientgooglesignin
 
-import android.content.ContentValues.TAG
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.credentials.CredentialManager
 import androidx.credentials.CustomCredential
 import androidx.credentials.GetCredentialRequest
-import androidx.credentials.GetCredentialResponse
 import androidx.credentials.PasswordCredential
 import androidx.credentials.PublicKeyCredential
-import androidx.credentials.exceptions.GetCredentialException
+import androidx.credentials.exceptions.NoCredentialException
 import androidx.lifecycle.lifecycleScope
-import com.example.clientgooglesignin.ui.theme.ClientGoogleSignInTheme
 import com.google.android.libraries.identity.googleid.GetSignInWithGoogleOption
 import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
 import com.google.android.libraries.identity.googleid.GoogleIdTokenParsingException
-import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
@@ -35,131 +31,70 @@ class MainActivity : ComponentActivity() {
 
         val button: Button = findViewById(R.id.button)
         button.setOnClickListener {
-            lifecycleScope.launch {
-                signIn()
-            }
+            googleLogin(this)
 
             Toast.makeText(this, "Button clicked!", Toast.LENGTH_SHORT).show()
         }
-        /*
-        enableEdgeToEdge()
-        setContent {
-            ClientGoogleSignInTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-         */
     }
 
-    private suspend fun signIn() {
-        /*
-        val googleIdOption: GetGoogleIdOption = GetGoogleIdOption.Builder()
-            .setFilterByAuthorizedAccounts(true)
-            .setServerClientId(WEB_CLIENT_ID)
-            .setAutoSelectEnabled(true)
-            // nonce string to use when generating a Google ID token
-            //.setNonce(nonce)
-            .build()
-         */
-        val signInWithGoogleOption: GetSignInWithGoogleOption = GetSignInWithGoogleOption.Builder(WEB_CLIENT_ID)
-            // nonce string to use when generating a Google ID token
-            //.setNonce(nonce)
+    private fun googleLogin(context: Context) {
+        val signInWithGoogleOption = GetSignInWithGoogleOption.Builder(WEB_CLIENT_ID)
             .build()
 
         val request: GetCredentialRequest = GetCredentialRequest.Builder()
             .addCredentialOption(signInWithGoogleOption)
             .build()
 
-        val myAuthManager = MyAuthManager(this)
-        val activityContext = this
+        val credentialManager = CredentialManager.create(context)
 
-        coroutineScope {
-            try {
-                val credentialManager = myAuthManager.getCredentialManager()
-
+        lifecycleScope.launch {
+            val loginFlow = flow {
                 val result = credentialManager.getCredential(
-                    request = request,
-                    context = activityContext
+                    context = context,
+                    request = request
                 )
-                handleSignIn(result)
-            }
-            catch (e: GetCredentialException) {
-                // Handle failure
-                val myError = e
-                Log.e(TAG, "" + myError.message)
+                //**// Boom! NoCredentialException: No credentials available**
+
+                emit(result.credential)
+            }.catch { e ->
+                if(e is NoCredentialException) {
+                    Log.e("TAG", "loginSuccess NoCredentialException", e)
+                }
+            }.collect {
+                loginSuccess(it)
             }
         }
     }
 
-    private fun handleSignIn(result: GetCredentialResponse) {
-        // Handle the successfully returned credential.
-        val credential = result.credential
-        val responseJson: String
-
-        when (credential) {
-
-            // Passkey credential
-            is PublicKeyCredential -> {
-                // Share responseJson such as a GetCredentialResponse to your server to validate and
-                // authenticate
-                responseJson = credential.authenticationResponseJson
-            }
-
-            // Password credential
+    private fun loginSuccess(credential: androidx.credentials.Credential) {
+        when(credential) {
             is PasswordCredential -> {
-                // Send ID and password to your server to validate and authenticate.
-                val username = credential.id
                 val password = credential.password
+                val id = credential.id
+                Log.e("TAG", "loginSuccess id: $id password: $password")
             }
-
-            // GoogleIdToken credential
+            is GoogleIdTokenCredential -> {
+                val idToken = credential.idToken
+                Log.e("TAG", "loginSuccess idToken: $idToken")
+            }
+            is PublicKeyCredential -> {
+                Log.e("TAG", "loginSuccess PublicKeyCredential")
+            }
             is CustomCredential -> {
-                if (credential.type == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL) {
-                    try {
-                        // Use googleIdTokenCredential and extract the ID to validate and
-                        // authenticate on your server.
-                        val googleIdTokenCredential = GoogleIdTokenCredential
-                            .createFrom(credential.data)
 
-                        // You can use the members of googleIdTokenCredential directly for UX
-                        // purposes, but don't use them to store or control access to user
-                        // data. For that you first need to validate the token:
-                        // pass googleIdTokenCredential.getIdToken() to the backend server.
-                        // see [validation instructions](https://developers.google.com/identity/gsi/web/guides/verify-google-id-token)
+                if(credential.type == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL) {
+                    try {
+
+                        val googleIdTokenCredential = GoogleIdTokenCredential.createFrom(credential.data)
+                        Log.e("TAG", "loginSuccess idToken: ${googleIdTokenCredential.idToken}  ${googleIdTokenCredential.displayName}")
                     } catch (e: GoogleIdTokenParsingException) {
-                        Log.e(TAG, "Received an invalid google id token response", e)
+                        e.printStackTrace()
                     }
-                } else {
-                    // Catch any unrecognized custom credential type here.
-                    Log.e(TAG, "Unexpected type of credential")
+                }
+                else {
+                    Log.e("TAG", "Unexcepted CustomCredential type: ${credential.type}")
                 }
             }
-
-            else -> {
-                // Catch any unrecognized credential type here.
-                Log.e(TAG, "Unexpected type of credential")
-            }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ClientGoogleSignInTheme {
-        Greeting("Android")
     }
 }
